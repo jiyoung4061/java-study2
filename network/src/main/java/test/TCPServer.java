41dc1dcf4c5d9911c18bc706955d054d24490073
jiyoung <jiyoung4061@naver.com>
2020-12-15 22:09:11 +0900

    2020.12.15 복습

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -16,15 +16,17 @@ public class TCPServer {
 		ServerSocket serverSocket = null;
 
 		try {
-			// 1. 서버 소켓 생성
+			// 1. 서버 소켓 생성 (서버 소켓 : 요청을 받아주는 소켓!)
 			serverSocket = new ServerSocket();
 
 			// 2. 바인딩(binding): Socket Address(IP Address + Port) => 노트 필기
+//			InetSocketAddress inetSocketAddress = new InetSocketAddress("127.0.0.1", 5000);
+//			serverSocket.bind(inetSocketAddress);
 			serverSocket.bind(new InetSocketAddress("127.0.0.1", 6000));
 
-//			System.out.println("blocking 안됨"); // blocking안되면 이거 출력됨
-			// 3. accept
-			Socket socket = serverSocket.accept(); // blocking(프로그램 멈춤)
+			// 3. accept : connect를 받아줌
+			Socket socket = serverSocket.accept(); // blocking(프로그램 멈춤) 왜? 누군가 connect를 해줘야 accept를 하니까 connect 요청 올때까지 잠시 멈춤
+			// System.out.println("blocking 안됨"); // blocking안되면 이거 출력됨
 
 			InetSocketAddress remoteInetSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
 			InetAddress remoteInetAddress = remoteInetSocketAddress.getAddress();
@@ -33,34 +35,35 @@ public class TCPServer {
 			System.out.println("[server] connected by client[" + remoteHostAddress + ":" + remotePort + "]");
 
 			try {
-				// 4. io stream 받아오기
+				// 4. IOStream 받아오기
 				InputStream is = socket.getInputStream();
 				OutputStream os = socket.getOutputStream();
 
 				while (true) {
 					// 5. 데이터 읽기
 					byte[] buffer = new byte[256];
-					int readByteCount = is.read(buffer); // blocking
+					int readByteCount = is.read(buffer); // blocking(쓸때까지 기다림)
 
 					if (readByteCount == -1) {
-						// clinet가 socket을 정상적으로 공유했다는 뜻(정상 종료)
+						// clinet가 socket을 정상적으로 종료했다는 뜻
 						System.out.println("[server] close by client");
 						break;
 					}
 
-					String data = new String(buffer, 0, readByteCount, "UTF-8");
+					// byte[] 배열을 String으로 인코딩하는 작업
+					String data = new String(buffer, 0, readByteCount, "UTF-8"); // buffer배열을 0부터 readByteCount만큼 utf-8로 인코딩해라
 					System.out.println("[server] receive: " + data);
 
 					// 6. 데이터 쓰기
-					os.write(data.getBytes("UTF-8"));
+					os.write(data.getBytes("UTF-8")); //String을 byte[]로 
 				}
 
 			} catch (SocketException e) {
 				// server 비정상 종료
 				System.out.println("[client] suddenly closed by server");
-			} catch (IOException e) {
+			} catch (IOException e) { //데이터 통신시 소켓에서 exception발생할 때
 				System.out.println("[server] error:" + e);
-			} finally {
+			} finally { 
 				try {
 					if (socket != null && !socket.isClosed())
 						socket.close();
@@ -69,11 +72,11 @@ public class TCPServer {
 				}
 			}
 
-		} catch (IOException e) { // server에서 exception발생시
+		} catch (IOException e) { // 서버 소켓에대한 exception
 			System.out.println("[server] error:" + e);
-		} finally {
+		} finally { // server socket close!
 			try {
-				if (serverSocket != null && !serverSocket.isClosed())
+				if (serverSocket != null && !serverSocket.isClosed())// null일 경우 exception발생, 또 닫힌 socket을 또 닫으려고하면 오류나므로!
 					serverSocket.close();
 			} catch (IOException e) {
 				e.printStackTrace();
